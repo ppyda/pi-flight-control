beed7aa74e5afad7e75ad4a3d2fc77946ce14372
Michael Muenzer <michael.muenzer@gmail.com>
2021-01-13 21:41:58 +0100

    Add websocket connection with mock implementation

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ Expect costs of about 100€ and ~10 hours to install & configure it.
 1. [Setup Raspberry Pi Zero W](https://michaelmuenzer.medium.com/get-started-with-your-raspberry-pi-zero-6cfc80321680)
 2. [Setup camera module](https://michaelmuenzer.medium.com/put-glasses-on-to-your-raspberry-pi-zero-8eea55eb36c9)
 3. [Connect pi-blaster and ESC](https://michaelmuenzer.medium.com/use-pi-blaster-to-run-esc-and-servo-motor-from-a-raspberry-pi-38aa1c7a1e6e)
-5. Control boat from within the web-browser
+5. [Control boat from within the web-browser](https://michaelmuenzer.medium.com/control-a-raspberry-powered-rc-boat-from-within-the-web-browser-aaab1e84e1a9)
 4. Setup Wifi AP
 
 
--- app.js
+++ app.js
@@ -9,17 +9,11 @@ app.set('view engine', 'html');
 
 var app_port=8082;
 app.get('/', function(req, res) {
-    res.render(__dirname + '/index.html', {localip:ip.address()});
+    res.render(__dirname + '/index.html', { localip: ip.address() });
 });
 app.listen(app_port);
 
 console.log('Web server listening, visit http://' + ip.address() + ':' + app_port);
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-// Main POST control
 
 var motor_pwm_pin = 17;
 var pwm_motor_init = 0.16;                           // neutral position
@@ -30,23 +24,116 @@ var pwm_motor_off = 0.0;
 var pwm_motor_min_limit = 0.1; // move backward slower than possible
 var pwm_motor_max_limit = 0.2; // move forward slower than possible
 
+var speed = 0;                                   // neutral position
+var default_speed_step_width = 1;
+var speed_num_steps = 20;
+var speed_min = (-1) * speed_num_steps / 2;      // max steps backward
+var speed_max = speed_num_steps / 2;             // max steps forward
+
 function speed2pwm(s) { 
-    pwm = pwm_motor_min + ((pwm_motor_max-pwm_motor_min) * ((s+speed_max) / speed_num_steps));
-    console.log('pwm calc: ' + pwm);
+    pwm = pwm_motor_min + ((pwm_motor_max - pwm_motor_min) * ((s + speed_max) / speed_num_steps));
 
     pwm = Math.min(pwm, pwm_motor_max_limit);
     pwm = Math.max(pwm, pwm_motor_min_limit);
 
-    console.log('pwm: ' + pwm);
-
+    console.log('speed pwm: ' + pwm);
     return pwm;
 }
 
-var speed = 0;                               // neutral position
-var speed_step_width = 1;
-var speed_num_steps = 20;
-var speed_min = (-1) * speed_num_steps/2;      // max steps backward
-var speed_max = speed_num_steps/2;             // max steps forward
+var servo_pwm_pin = 18;
+var pwm_servo_min = 0.05;
+//var pwm_servo_neutral = 0.1;
+var pwm_servo_max = 0.15;
+
+var angle = 0;
+var default_angle_step_width = 15;
+var angle_min = -45;
+var angle_max = 45;
+
+function angle2pwm(a) {
+    pwm = pwm_servo_min + (pwm_servo_max - pwm_servo_min) * (a + 45) / 90;
+    
+    console.log('angle pwm:' + pwm);
+    return pwm;
+}
+
+// Main POST control
+
+app.post('/start', function (req, res) {
+    start();
+    res.end();
+});
+
+app.post('/stop', function (req, res) {
+    stop();
+    res.end();
+});
+
+app.post('/forward', function (req, res) {
+    moveForward(default_speed_step_width)
+    res.end();
+});
+
+app.post('/backward', function (req, res) {
+    moveBackward(default_speed_step_width)
+    res.end();
+});
+
+app.post('/left', function (req, res) {
+    moveLeft(default_angle_step_width);
+    res.end();
+});
+
+app.post('/right', function (req, res) {
+    moveRight(default_angle_step_width);
+    res.end();
+});
+
+function start() {
+    console.log('start');
+
+    speed = 0;
+    piblaster.setPwm(motor_pwm_pin, pwm_motor_init);
+}
+
+function stop() {
+    console.log('stop');
+    
+    speed = 0;
+    piblaster.setPwm(motor_pwm_pin, pwm_motor_off);
+    
+    angle = 0;
+    piblaster.setPwm(servo_pwm_pin, angle2pwm(angle));
+}
+
+function moveForward(step_width) {
+    speed = speed + step_width;
+
+    speed = Math.min(speed, speed_max);
+    speed = Math.max(speed, speed_min);
+
+    console.log('move forward: ' + speed);
+
+    piblaster.setPwm(motor_pwm_pin, speed2pwm(speed));
+}
+
+function moveBackward(step_width) {
+    var prev_speed = speed;
+    speed = speed - step_width;
+    
+    speed = Math.min(speed, speed_max);
+    speed = Math.max(speed, speed_min);
+
+    console.log('move backward: ' + speed);
+    
+    piblaster.setPwm(motor_pwm_pin, speed2pwm(speed));
+
+    // Double-click procedure.
+    // Can also be done manually, depending on your preference and need
+    if (prev_speed == 0) {
+      runDoubleClickProcedure();
+    }
+}
 
 async function runDoubleClickProcedure() {
     console.log('Run double-click procedure automatically');
@@ -56,109 +143,81 @@ async function runDoubleClickProcedure() {
     piblaster.setPwm(motor_pwm_pin, speed2pwm(speed));
 }
 
-app.post('/backward', function (req, res) {
-    var prev_speed = speed;
-    speed = speed-speed_step_width;
-    
-    speed = Math.min(speed, speed_max);
-    speed = Math.max(speed, speed_min);
-
-    console.log('press backward: ' + speed);
-    
-    piblaster.setPwm(motor_pwm_pin, speed2pwm(speed));
-
-    // Double-click procedure.
-    // Can also be done manually, depending on your preference and need
-    if (prev_speed == 0) {
-      runDoubleClickProcedure();
-    }
-
-    res.end();
-});
-
-app.post('/forward', function (req, res) {
-    speed = speed + speed_step_width;
-
-    speed = Math.min(speed, speed_max);
-    speed = Math.max(speed, speed_min);
-
-    console.log('press forward: ' + speed);
-
-    piblaster.setPwm(motor_pwm_pin, speed2pwm(speed));
-    res.end();
-});
-
-var servo_pwm_pin = 18;
-var pwm_servo_min = 0.05;
-var pwm_servo_neutral = 0.1;
-var pwm_servo_max = 0.15;
-
-var angle_max = 45;
-var angle_min = -45;
-
-function angle2pwm(a) {
-    pwm = pwm_servo_min + (pwm_servo_max-pwm_servo_min) * (a+45) / 90;
-    //console.log('set pwm:' + pwm);
-    return pwm;
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-var angle = 0;
-var angle_step_width = 15;
-app.post('/right', function (req, res) {
-    angle = angle+angle_step_width;
-
-    angle = Math.min(angle, angle_max);
-    angle = Math.max(angle, angle_min);
-    
-    piblaster.setPwm(servo_pwm_pin, angle2pwm(angle));
-    console.log('press right: ' + angle);
-    res.end();
-});
-
-app.post('/left', function (req, res) {
-    angle = angle - angle_step_width;
+function moveLeft(step_width) {
+    angle = angle - step_width;
 
     angle = Math.min(angle, angle_max);
     angle = Math.max(angle, angle_min);
     
     piblaster.setPwm(servo_pwm_pin, angle2pwm(angle));
     console.log('press left: '+ angle);
-    res.end();
-});
+}
 
-app.post('/start', function (req, res) {
-    speed = 0;
-    console.log('start');
-    piblaster.setPwm(motor_pwm_pin, pwm_motor_init);
-    res.end();
-});
+function moveRight(step_width) {
+    angle = angle + step_width;
 
-app.post('/stop', function (req, res) {
-    console.log('stop');
+    angle = Math.min(angle, angle_max);
+    angle = Math.max(angle, angle_min);
     
-    speed = 0;
-    piblaster.setPwm(motor_pwm_pin, pwm_motor_off);
-    
-    angle = 0;
     piblaster.setPwm(servo_pwm_pin, angle2pwm(angle));
-    res.end();
-});
-
-// If we lose comms, set the servos to neutral
-function emergencyStop() {
-    speed = 0;
-    piblaster.setPwm(motor_pwm_pin, pwm_motor_off);
-    
-    angle = 0;
-    piblaster.setPwm(servo_pwm_pin, angle2pwm(angle));
-    
-    console.log('### EMERGENCY STOP - signal lost or shutting down');
+    console.log('press right: ' + angle);
 }
 
 // User hits Ctrl+C
 process.on('SIGINT', function() {
-    emergencyStop();
+    stop();
     console.log("\nGracefully shutting down from SIGINT (Ctrl-C)");
     
     return process.exit();
 });
+
+var ws_port=3002;
+var user_id;
+var wss = require("ws").Server({server: server, port: ws_port});
+wss.on("connection", function (ws) {
+
+    console.log("Websocket connection opened");
+
+    var timestamp = new Date().getTime();
+    user_id = timestamp;
+
+    ws.send(JSON.stringify({msgType:"onOpenConnection", msg:{ connectionId: user_id }}));
+
+    ws.on("message", function (data, flags) {
+        var client_message = data.toString() + "";
+        console.log("Websocket received a message: " + client_message + " (" + typeof(client_message) + ")");
+
+        if (!(client_message === 'undefined')) {
+            if (client_message.indexOf("start") == 0) {
+                start();
+            }
+            else if (client_message.indexOf("stop") == 0) {
+                stop();
+            }
+            else if (client_message.indexOf("angle:") == 0) {
+                var angle_str = client_message.split(":")[1].trim();
+                console.log("normalized angle: " + angle_str);
+                normalized_angle = parseFloat(angle_str);
+                //TODO: Convert normalized value to angle and pass on
+            } 
+            else if (client_message.indexOf("speed:") == 0){
+                var speed_str = client_message.split(":")[1].trim();
+                console.log("normalized speed: " + speed_str);
+                normalized_speed = parseFloat(speed_str);
+                //TODO: Convert normalized value to speed and pass on
+            }
+        }
+
+        ws.send(JSON.stringify({ msg:{ connectionId: user_id } }));
+    });
+
+    ws.on("close", function () {
+        console.log("Websocket connection closing");
+        stop();
+    });
+});
+console.log("Websocket server created");
