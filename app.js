a6ef010893f64d302f0e40bab7984bbfae7c4bb5
bolanren <piotr.pyda@icloud.com>
2022-04-23 20:19:28 +0200

    change port

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ wsServer.on('connection', socket => {
 // `server` is a vanilla Node.js HTTP server, so use
 // the same ws upgrade process described here:
 // https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
-const server = app.listen(3000, '127.0.0.1', () => {
+const server = app.listen(3000, '192.168.1.118', () => {
     console.log('Server is up.')
 });
 server.on('upgrade', (request, socket, head) => {
