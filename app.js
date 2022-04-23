838043ef976662d656a2573e574a991efc64ee44
bolanren <piotr.pyda@icloud.com>
2022-04-23 20:17:24 +0200

    chage server listen config

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ wsServer.on('connection', socket => {
 // `server` is a vanilla Node.js HTTP server, so use
 // the same ws upgrade process described here:
 // https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
-const server = app.listen(3000, 'localhost', () => {
+const server = app.listen(3000, '127.0.0.1', () => {
     console.log('Server is up.')
 });
 server.on('upgrade', (request, socket, head) => {
