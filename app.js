f1e92d6dd05ca9fc2a5beb32871fe98294dd6e7a
bolanren <piotr.pyda@icloud.com>
2022-04-26 21:19:05 +0200

    hostname

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ wsServer.on('connection', socket => {
 // `server` is a vanilla Node.js HTTP server, so use
 // the same ws upgrade process described here:
 // https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
-const server = app.listen(3000, 'raspberrypi', () => {
+const server = app.listen(3000, 'raspberrypi.fritz.box', () => {
     console.log('Server is up.')
 });
 server.on('upgrade', (request, socket, head) => {
