c2be53bf91ce8b4719714c2acc8c8c1eb03c0fee
bolanren <piotr.pyda@icloud.com>
2022-04-26 21:16:48 +0200

    change hostname

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ wsServer.on('connection', socket => {
 // `server` is a vanilla Node.js HTTP server, so use
 // the same ws upgrade process described here:
 // https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
-const server = app.listen(3000, '192.168.1.118', () => {
+const server = app.listen(3000, 'raspberrypi', () => {
     console.log('Server is up.')
 });
 server.on('upgrade', (request, socket, head) => {
